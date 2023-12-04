761dd323e4e73b225274ec1f3bb3c478b1d67385
James Warner <james.warner@indianhills.edu>
2023-12-04 08:31:30 -0600

    simplifying

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,8 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MainTest {
